62dbf6d0ccb058d46835ea7325c2c3a3af0d8b26
mliboz <maxoulib@gmail.com>
2022-05-27 14:08:15 +0200

    refactor Array class

--- d07/ex02/includes/Array.class.hpp
+++ d07/ex02/includes/Array.class.hpp
@@ -20,50 +20,16 @@ class Array
 {
 	public:
 		
-		Array() : _size(1) {this->_array = new T();};
+		Array();
+		Array( unsigned int n );
+		Array( Array<T> & var );
+		~Array();
 
-		Array( unsigned int n ) : _size(n)
-		{
-			this->_array = new T[n];
-			for (unsigned int i = 0; i < n; i++)
-			{
-				this->_array[i] = 0;
-			}
-		}
+		Array<T> &	operator=( Array<T> & var );
+		T			operator[](unsigned int index) const;
+		T &			operator[](unsigned int index);
 
-		Array( Array<T> & var )
-		{
-			this->_array = new T[var.size()];
-			*this = var;
-		}
-
-		~Array() { delete [] this->_array; }
-
-		Array<T> & operator=( Array<T> & var )
-		{
-			for (unsigned int i = 0; i < var.size() - 1; i++)
-			{
-				this->_array[i] = var[i];
-			}
-			this->_size = var.size();
-			return (*this);
-		}
-		
-		T operator[](unsigned int index) const
-		{
-			if (index >= this->size())
-				throw (Array<T>::IndexOutOfBoundsException());
-			return (this->_array[index]);
-		}
-		
-		T & operator[](unsigned int index)
-		{
-			if (index >= this->size())
-				throw (Array<T>::IndexOutOfBoundsException());
-			return *(this->_array + index);
-		}
-
-		unsigned int	size() const { return (this->_size); }
+		unsigned int	size() const;
 
 		class IndexOutOfBoundsException : public std::exception
 		{
@@ -78,6 +44,66 @@ class Array
 		T			*_array;
 		unsigned int _size;	
 };
+
+
+template<typename T>
+Array<T>::Array() : _size(1)
+{
+	this->_array = new T();
+}
+
+template<typename T>
+Array<T>::Array( unsigned int n ) : _size(n)
+{
+	this->_array = new T[n];
+	for (unsigned int i = 0; i < n; i++)
+	{
+		this->_array[i] = 0;
+	}
+}
+
+template<typename T>
+Array<T>::Array( Array<T> & var )
+{
+	this->_array = new T[var.size()];
+	*this = var;
+}
+
+template<typename T>
+Array<T>::~Array()
+{
+	delete [] this->_array;
+}
+
+template<typename T>
+Array<T> &  Array<T>::operator=( Array<T> & var )
+{
+	for (unsigned int i = 0; i < var.size() - 1; i++)
+	{
+		this->_array[i] = var[i];
+	}
+	this->_size = var.size();
+	return (*this);
+}
+
+template<typename T>
+T Array<T>::operator[](unsigned int index) const
+{
+	if (index >= this->size())
+		throw (Array<T>::IndexOutOfBoundsException());
+	return (this->_array[index]);
+}
+
+template<typename T>
+T & Array<T>::operator[](unsigned int index)
+{
+	if (index >= this->size())
+		throw (Array<T>::IndexOutOfBoundsException());
+	return *(this->_array + index);
+}
+
+template<typename T>
+unsigned int	Array<T>::size() const { return (this->_size); }
 
 template<typename T>
 std::ostream & operator<<(std::ostream & o, Array<T> const & var)
