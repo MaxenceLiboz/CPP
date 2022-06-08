6e5b8a67bf0111de9257f653ba3e803c5d0e634d
mliboz <maxoulib@gmail.com>
2022-06-08 12:40:46 +0200

    Add iterator to dqueu container

--- d08/ex02/includes/MutantStack.hpp
+++ d08/ex02/includes/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 10:21:59 by mliboz            #+#    #+#             */
-/*   Updated: 2022/06/07 11:38:42 by mliboz           ###   ########.fr       */
+/*   Updated: 2022/06/08 12:31:42 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,14 @@ template<typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
+		typedef typename MutantStack<T>::container_type::iterator iterator;
+		
 		MutantStack(){};
 		MutantStack( MutantStack<T> const & var );
 		~MutantStack(){};
 
+		MutantStack<T>::iterator begin(){ return (this->c.begin()); }
+		MutantStack<T>::iterator end(){ return (this->c.end()); }
 		MutantStack<T> & operator=( MutantStack<T> const & var );
 };
 
--- d08/ex02/main.cpp
+++ d08/ex02/main.cpp
@@ -6,20 +6,59 @@
 /*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 10:29:47 by mliboz            #+#    #+#             */
-/*   Updated: 2022/06/07 11:36:41 by mliboz           ###   ########.fr       */
+/*   Updated: 2022/06/08 12:39:54 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
-int	main(void)
+int main()
 {
-	MutantStack<int> stack;
-	
-	stack.push(10);
-	stack.push(14);
-	stack.push(15);
-	std::cout << stack.top() << std::endl;
-	std::cout << stack.top() << std::endl;
-	return(0);
+	std::cout << "MutantStack output: " << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::cout << "List output: " << std::endl;
+	std::list<int> lstack;
+	lstack.push_back(5);
+	lstack.push_back(17);
+	std::cout << lstack.back() << std::endl;
+	lstack.pop_back();
+	std::cout << lstack.size() << std::endl;
+	lstack.push_back(3);
+	lstack.push_back(5);
+	lstack.push_back(737);
+	//[...]
+	lstack.push_back(0);
+	std::list<int>::iterator it1 = lstack.begin();
+	std::list<int>::iterator ite1 = lstack.end();
+	++it1;
+	--it1;
+	while (it1 != ite1)
+	{
+		std::cout << *it1 << std::endl;
+		++it1;
+	}
+	std::list<int> s1(lstack);
+	return 0;
 }
